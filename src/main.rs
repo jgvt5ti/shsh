9ca3e2cdc42e5362c169831a675da59fa01a44b0
jgvt5 <renf.core2duo@gmail.com>
2020-05-06 05:29:37 +0900

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,23 @@ fn main() {
         let stdin = io::stdin();
         let mut handle = stdin.lock();
         match handle.read_line(&mut buf) {
-            Ok(0) => break, // is EOF?
+            Ok(0) => break,
             Ok(_) => {
-                let args: Vec<&str> = buf.trim().split_whitespace().collect();
-                invoke_cmd(&args);
+                buf.pop(); // 改行削除
+                if buf == "exit" {
+                    break;
+                }
+                if !buf.is_empty() {
+                    invoke_cmd(&buf);
+                }
             }
             Err(e) => println!("Error: {}", e),
         }
     }
 }
 
-fn invoke_cmd(args: &Vec<&str>) {
+fn invoke_cmd(buf: &str) {
+    let mut inputs: Vec<&str> = buf.split('|').collect();
     match fork() {
         Ok(ForkResult::Parent { child }) => match waitpid(child, None).expect("wait_pid failed") {
             WaitStatus::Exited(_, status) => {
@@ -35,17 +41,53 @@ fn invoke_cmd(args: &Vec<&str>) {
             _ => println!("Abnormal exit!"),
         },
         Ok(ForkResult::Child) => {
-            if args.len() != 0 {
-                let cstring_args: Vec<CString> = args
-                    .iter()
-                    .map(|s| CString::new(s.clone()).unwrap())
-                    .collect();
-                let exec_args: Vec<&CStr> = cstring_args.iter().map(AsRef::as_ref).collect();
-
-                execv(exec_args[0], exec_args.as_ref()).expect("Execution failed");
-            }
-            std::process::exit(0);
+            rec_cmd(&mut inputs);
         }
         Err(_) => println!("Fork failed"),
     }
+}
+
+fn rec_cmd(inputs: &mut Vec<&str>) {
+    // 残りコマンドが1つ
+    if inputs.len() == 1 {
+        exec_cmd(inputs[0]);
+    } else {
+        let (pin, pout) = pipe().unwrap();
+        match fork() {
+            Ok(ForkResult::Parent { child: _ }) => {
+                // stdin -> pin
+                close(pout).expect("Pipe Error");
+                close(0).expect("Pipe Error");
+                dup2(pin, 0).expect("Pipe Error");
+                close(pin).expect("Pipe Error");
+                // 右のコマンドをexecして死ぬ
+                let input = inputs.pop().expect("Pipe Error");
+                exec_cmd(input);
+            }
+            Ok(ForkResult::Child) => {
+                // stdout -> pout
+                close(pin).expect("Pipe Error");
+                close(1).expect("Pipe Error");
+                dup2(pout, 1).expect("Pipe Error");
+                close(pout).expect("Pipe Error");
+                // 左のコマンド
+                inputs.pop();
+                rec_cmd(inputs);
+            }
+            Err(e) => {
+                println!("Error occurred:{}", e);
+                std::process::exit(1);
+            }
+        }
+    }
+}
+
+fn exec_cmd(input: &str) {
+    let args: Vec<&str> = input.trim().split_whitespace().collect();
+    let cstring_args: Vec<CString> = args
+        .iter()
+        .map(|s| CString::new(s.clone()).unwrap())
+        .collect();
+    let exec_args: Vec<&CStr> = cstring_args.iter().map(AsRef::as_ref).collect();
+    execv(exec_args[0], exec_args.as_ref()).expect("Execution failed");
 }
